a85b52cafc6863c8cd724381924951807b02c560
Carlos <amaroinacio2500@gmail.com>
2023-10-04 01:43:18 -0300

    desafio concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,11 +3,18 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama 
+        private string Modelo {get;set;}
+        private string Imei {get;set;}
 
-        public Smartphone(string numero)
+        private int Memoria {get;set;}
+
+        public Smartphone(string numero,string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
 
--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Nokia:");
+Nokia nokia = new Nokia(numero:"12345",modelo:"modelo 1", imei:"11111111",memoria:64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Iphone:");
+
+Iphone iphone = new Iphone(numero:"4568",modelo:"modelo 2", imei:"2222222",memoria:128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
